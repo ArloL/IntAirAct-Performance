e3a0a5ec3cf6e026fa7302629f949a29a1c933e6
Arlo O'Keeffe <MailToArlo@googlemail.com>
2012-06-21 09:07:36 -0600

    Added structure SQL file

---
+++ testing/jmeter.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 21, 2012 at 09:07 AM
+-- Server version: 5.1.62-log
+-- PHP Version: 5.3.12
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `jmeter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `results`
+--
+
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `test_id` int(11) unsigned NOT NULL,
+  `timeStamp` datetime NOT NULL,
+  `elapsed` int(11) NOT NULL,
+  `label` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `responseCode` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `responseMessage` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `threadName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `dataType` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `success` tinyint(1) NOT NULL,
+  `bytes` int(11) NOT NULL,
+  `Latency` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `test_id` (`test_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tests`
+--
+
+CREATE TABLE IF NOT EXISTS `tests` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `executed` datetime NOT NULL,
+  `users` int(11) unsigned DEFAULT NULL,
+  `host` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `runtime` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
